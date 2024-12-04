754b1cf755b5525c7267e5a997b8d870155934ee
Hongyi-2024 <liangyuchao@gmail.com>
2024-12-04 04:08:49 +0000

    Co-authored-by: Computhink-Alfred <Computhink-Alfred@users.noreply.github.com>

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -1,7 +1,3 @@
-# Write all your codes for Day 2 here.
-# REMEMBER to change main.py to import this file.
-# You can COMMENT out the previous task before going on to the next task
-print("hello from day2")
 
 
 
